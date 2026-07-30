15f22d995fc99172f80bcc754b37114aedf3d46b
Deepak <deepakjalmoori@gmail.com>
2026-07-30 17:03:05 +0530

    Fix CORS configuration for production

--- src/app.js
+++ src/app.js
@@ -1,46 +1,32 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
-const User = require("./models/user");
 const cookieParser = require("cookie-parser");
 const authRouter = require("./routes/auth");
 const requestRouter = require("./routes/request");
 const profileRouter = require("./routes/profile");
 const userRouter = require("./routes/user");
+const chatRouter = require("./routes/chat");
 const cors = require("cors");
 const http = require("http");
 const initializeSocket = require("./utils/socket");
-const chatRouter = require("./routes/chat");
 
 require("dotenv").config();
+
 const allowedOrigins = [
   "http://localhost:5173",
   "https://dev-tinder-ui-seven.vercel.app",
 ];
 
-const corsOptions = {
-  origin: allowedOrigins,
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-};
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
-app.use(cors(corsOptions));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", corsOptions.origin);
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET,POST,PUT,PATCH,DELETE,OPTIONS",
-  );
-  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(204);
-  }
-
-  next();
-});
 app.use(express.json());
 app.use(cookieParser());
 
@@ -51,16 +37,17 @@ app.use("/", userRouter);
 app.use("/", chatRouter);
 
 const server = http.createServer(app);
+
 initializeSocket(server);
 
 connectDB()
   .then(() => {
     console.log("Database connection established...");
     server.listen(process.env.PORT, () => {
-      console.log("Server is running on port " + process.env.PORT + "!!!");
+      console.log(`Server is running on port ${process.env.PORT}!!!`);
     });
   })
   .catch((err) => {
-    console.log("Something went wrong!!!!");
+    console.error("Something went wrong!!!!");
     console.error(err);
   });
